05c095d671cb5af07cfa3614335b8a39ac0ff173
devil-hades <ngoduykhanh2001@gmail.com>
2021-04-02 03:02:55 +0700

    complete zoom img

--- assets/JS/products.js
+++ assets/JS/products.js
@@ -1,5 +1,30 @@
 $(document).ready(function () {
-    $('.configuration-list li').click(function (e) { 
+    $('.configuration-list li').click(function (e) {
         $(this).addClass('active').siblings().removeClass('active');
     });
-});
+
+
+    // thay đổi ảnh chính
+    $('.img-list li > img').click(function (e) {
+        var smallImgs = $(this).attr('src');
+        $('.img-main a > img').css({
+            opacity: 0.25
+        });
+        setTimeout(function () {
+            $('.img-main a > img').attr('src', smallImgs);
+            $('.img-main a > img').css({
+                opacity: 1,
+                visibility: 'visible'
+            });
+            $('.img-main > a').attr('href', smallImgs);
+           
+        }, 200)
+    });
+
+    $(".img-main a > img").imagezoomsl({
+        zoomrange: [3, 3],
+        magnifycursor: 'all-scroll',
+    });
+
+
+})
--- assets/JS/slides.js
+++ assets/JS/slides.js
@@ -1,38 +1,38 @@
-
 $(document).ready(function () {
 
-   // slide show
-   var owl = $('.slides-show');
-   owl.owlCarousel({
-       items: 1,
-       loop: true,
-       margin: 10,
-       autoplay: true,
-       autoplayTimeout: 3000,
-       autoplayHoverPause: true
-   });
-   $('.play').on('click', function () {
-       owl.trigger('play.owl.autoplay', [1000])
-   })
-   $('.stop').on('click', function () {
-       owl.trigger('stop.owl.autoplay')
-   })
-   var owl = $('.accessories');
-   owl.owlCarousel({
-       items: 4,
-       loop: true,
-       margin: 10,
-       autoplay: false,
-       autoplayTimeout: 1000,
-       dots: false,
-       autoplayHoverPause: true
-   });
-   $('.play').on('click', function () {
-       owl.trigger('play.owl.autoplay', [1000])
-   })
-   $('.stop').on('click', function () {
-       owl.trigger('stop.owl.autoplay')
-   })
+    // slide show
+    var owl = $('.slides-show');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
+    var owl = $('.accessories');
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: false,
+        autoplayTimeout: 1000,
+        dots: false,
+        autoplayHoverPause: true
+    });
+   
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
 
 
 
