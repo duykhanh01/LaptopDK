5d13f7c2f4b2058d0aecb3306bbe3c818094baf8
devil-hades <ngoduykhanh2001@gmail.com>
2021-04-09 23:41:19 +0700

    update cart

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -21,23 +21,74 @@ $(document).ready(function () {
         $(button).click(removeCartItem);
     }
     var quantityInputs = $('.cart-quantity-input');
-    for (var i = 0; i <quantityInputs.length; i++) {
+    for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i];
-        $(input).change(quantityChanged)         
+        $(input).change(quantityChanged)
     }
-    function removeCartItem(event){
+
+    var addToCartButtons = $('.btn-buy-now');
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i];
+        $(button).click(addToCartClicked);
+    }
+
+
+
+
+    function removeCartItem(event) {
         var buttonClicked = event.target;
         $(buttonClicked).parent().parent().remove();
         updateCartTotal();
     }
 
-    function quantityChanged(event){
+    function quantityChanged(event) {
         var input = event.target;
         //kiem tra xem co phai la so hoac be hon 1 k
-        if(isNaN(input.value) || input.value<=0) {
-            input.value =1;
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1;
         }
         updateCartTotal();
+    }
+
+    function addToCartClicked(event) {
+        var button = event.target;
+        var shopItem = button.parentElement.parentElement.parentElement.parentElement;
+        var title = shopItem.getElementsByClassName('info-name')[0].innerText;
+        var price = shopItem.getElementsByClassName('info-price')[0].innerText;
+        var imgSrc = shopItem.getElementsByClassName('img-src')[0].src;
+        // var shopItem = $(button).parent().parent().parent().parent();
+        // var title = $('abc, .info-name').text();
+        // var price = $('shopItem, .info-price').text();
+        // var imgSrc = $('shopItem, .img-main a .img-src').attr('src');
+        addItemToCart(title, price, imgSrc);
+    }
+
+    function addItemToCart(title, price, imgSrc) {
+        var cartRow = document.createElement('div');
+        cartRow.classList.add('cart-row');
+        var cartItems = document.getElementsByClassName('cart-items')[0];
+        var cartItemsNames = document.getElementsByClassName('cart-item-title');
+        // for (var i = 0; i < cartItemsNames.length; i++) {
+        //     if(cartItemsNames[i].innerText == title){
+        //         alert('Sản phẩm này đã được thêm vào gỏ hàng!');
+        //         return;
+        //     }
+        // }
+        var cartRowContens = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imgSrc}">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+        cartRow.innerHTML = cartRowContens;
+        cartItems.append(cartRow);
+        updateCartTotal();
+        cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+        cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
     }
 
     function updateCartTotal() {
@@ -48,12 +99,13 @@ $(document).ready(function () {
             var cartRow = cartRows[i]
             var priceElement = cartRow.getElementsByClassName('cart-price')[0]
             var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-            var price = priceElement.innerText.replace('Đ' , '').split('.').join(''); // remove 'Đ' và dots
+            var price = priceElement.innerText.replace('Đ', '').split('.').join(''); // remove 'Đ' và dots
             var quantity = quantityElement.value;
             total = total + (price * quantity);
         }
         $('.cart-total-price').first().text(numberWithCommas(total) + 'Đ');
     }
+
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
         // \B giữ cho regax k đặt dấu chấm ở đầu chuỗi
@@ -63,4 +115,11 @@ $(document).ready(function () {
         // ?!\d một khẳng định phủ định để đảm bảo rằng điểm đó chỉ có đúng bội số của 3 chữ số. Biểu thức thay thế đặt dấu chấm ở đó. 
     }
 
+    $('.btn-cart-next').click(function () {
+      
+        $('.nav-link.active').parent().next('li').find('button').trigger('click');
+    });
+    $('.btn-cart-prev').click(function () {
+        $('.nav-link.active').parent().prev('li').find('button').trigger('click');
+    });
 });
